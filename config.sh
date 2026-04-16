2c1dff4004d9d6460e62ddcd3faa48a9c663b24b
nazephyrus <126683488+naden01@users.noreply.github.com>
2026-04-16 06:29:13 -0700

    update

--- config.sh
+++ config.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Kernel name
-KERNEL_NAME="SuiKernel"
+KERNEL_NAME="NzE-Gaming-kernel"
 # Kernel Build variables
-USER="KanagawaYamada"
-HOST="HoshimachiSuisei"
+USER="Nze"
+HOST="Nzeus"
 TIMEZONE="Asia/Jakarta"
 # AnyKernel
 ANYKERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-anykernel"
 ANYKERNEL_BRANCH="gki"
 # Kernel Source
-KERNEL_REPO="https://github.com/LoggingNewMemory/SuiKernel-android12-5.10"
+KERNEL_REPO="https://github.com/naden01/SuiKernel-android12-5.10"
 KERNEL_BRANCH="suikernel-main"
 KERNEL_DEFCONFIG="gki_defconfig"
 # Release repository
-GKI_RELEASES_REPO="https://github.com/LoggingNewMemory/SuiKernel-Release"
+GKI_RELEASES_REPO="https://github.com/naden01/SuiKernel-Release"
 # Clang
 CLANG_URL="https://github.com/LineageOS/android_prebuilts_clang_kernel_linux-x86_clang-r416183b"
 CLANG_BRANCH="lineage-20.0"
